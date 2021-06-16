d209abf93d3898b71837a30ff59f78b6fc351af3
Davi Lima <davim5@hotmail.com>
2021-06-16 07:41:26 -0300

    Validando autenticacao (Server)

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,8 @@
 import { useContext, useEffect } from "react"
 import { AuthContext } from "../context/AuthContext"
-import { api } from "../services/api";
+import { setupAPIClient } from "../services/api";
+import { api } from "../services/apiClient";
+import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard(){
 
@@ -9,10 +11,19 @@ export default function Dashboard(){
     useEffect(() =>{
         api.get('/me')
         .then(response => console.log(response))
-        .catch(err => console.log(err))
     },[])
 
     return(
         <h1>Dashboard: {user?.email} </h1>
     )
 }
+
+export const getServerSideProps = withSSRAuth( async (ctx) => {
+    
+    const apiClient = setupAPIClient(ctx);
+    const reponse = await apiClient.get('/me');
+    
+    return {
+        props:{}
+    }
+})
--- src/services/api.ts
+++ src/services/api.ts
@@ -2,78 +2,87 @@ import axios, {AxiosError} from 'axios';
 import { parseCookies, setCookie } from 'nookies';
 import { signOut } from '../context/AuthContext';
 
-let cookies = parseCookies();
 let isRefreshing = false;
 let failedRequestsQueue: { onSuccess: (token: string) => void; onFailure: (err: AxiosError<any>) => void; }[] = [];
 
-export const api = axios.create({
-    baseURL: 'http://localhost:3333',
-    headers: {
-        Authorization: `Bearer ${cookies['authrocket.token']}`
-    }
-});
+export function setupAPIClient(ctx = undefined){
+    let cookies = parseCookies(ctx);
 
-api.interceptors.response.use(response => {
-    return response;
-}, (error: AxiosError) => {
-    if(error.response?.status === 401) {
-        if(error.response.data?.code === 'token.expired') {
-            // renovar o token
-            cookies = parseCookies(); 
-
-            const { 'authrocket.refreshToken': refreshToken } = cookies;
-
-            const originalConfig = error.config;
-
-            if(!isRefreshing){
-                isRefreshing = true;
-
-                api.post('/refresh', {
-                    refreshToken,
-                }).then( response => {
-                    const { token } = response.data;
-    
-                    setCookie(undefined, 'authrocket.token', token,{
-                        maxAge: 60 * 60 * 24 * 30,
-                        path: '/',
-                    });
-        
-                    setCookie(undefined, 'authrocket.refreshToken', response.data.refreshToken, {
-                        maxAge: 60 * 60 * 24 * 30,
-                        path: '/',
-                    });
-    
-                    api.defaults.headers['Authorization'] = `Bearer ${token}`;
-
-                    failedRequestsQueue.forEach(request => request.onSuccess(token));
-                    failedRequestsQueue = [];
-                }).catch(err => {
-                    failedRequestsQueue.forEach(request => request.onFailure(err));
-                    failedRequestsQueue = [];
-                }).finally(()=>{
-                    isRefreshing = false;
-                });
-            }
-
-            return new Promise((resolve, reject) => {
-                failedRequestsQueue.push({
-                    onSuccess: (token:string) => {
-                        originalConfig.headers['Authorization'] = `Bearer ${token}`;
-
-                        resolve(api(originalConfig));
-                    },
-                    onFailure: (err: AxiosError) => {
-                        reject(err);
-                    },
-                })
-            });
-        } else {
-            // deslogar usuário
-            if(error.response.status === 401) {
-                signOut();
-            }
+    const api = axios.create({
+        baseURL: 'http://localhost:3333',
+        headers: {
+            Authorization: `Bearer ${cookies['authrocket.token']}`
         }
+    });
+    
+    api.interceptors.response.use(response => {
+        return response;
+    }, (error: AxiosError) => {
+        if(error.response?.status === 401) {
+            if(error.response.data?.code === 'token.expired') {
+                // renovar o token
+                cookies = parseCookies(ctx); 
+    
+                const { 'authrocket.refreshToken': refreshToken } = cookies;
+    
+                const originalConfig = error.config;
+    
+                if(!isRefreshing){
+                    isRefreshing = true;
+    
+                    api.post('/refresh', {
+                        refreshToken,
+                    }).then( response => {
+                        const { token } = response.data;
+        
+                        setCookie(ctx, 'authrocket.token', token,{
+                            maxAge: 60 * 60 * 24 * 30,
+                            path: '/',
+                        });
+            
+                        setCookie(ctx, 'authrocket.refreshToken', response.data.refreshToken, {
+                            maxAge: 60 * 60 * 24 * 30,
+                            path: '/',
+                        });
+        
+                        api.defaults.headers['Authorization'] = `Bearer ${token}`;
+    
+                        failedRequestsQueue.forEach(request => request.onSuccess(token));
+                        failedRequestsQueue = [];
+                    }).catch(err => {
+                        failedRequestsQueue.forEach(request => request.onFailure(err));
+                        failedRequestsQueue = [];
+    
+                        if(process.browser) {
+                            signOut();
+                        }
+                    }).finally(()=>{
+                        isRefreshing = false;
+                    });
+                }
+    
+                return new Promise((resolve, reject) => {
+                    failedRequestsQueue.push({
+                        onSuccess: (token:string) => {
+                            originalConfig.headers['Authorization'] = `Bearer ${token}`;
+    
+                            resolve(api(originalConfig));
+                        },
+                        onFailure: (err: AxiosError) => {
+                            reject(err);
+                        },
+                    })
+                });
+            } else {
+                // deslogar usuário
+                if(process.browser){
+                    signOut();
+                }
+            }
+    
+        }
+        return Promise.reject(error);
+    });
 
-    }
-    return Promise.reject(error);
-})
+    return api;
+}
