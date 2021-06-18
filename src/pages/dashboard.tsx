37b2e75ded25c4005cd9531f46e967bf204e7672
Davi Lima <davim5@hotmail.com>
2021-06-18 19:45:15 -0300

    Criando hook de permissao

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,12 +1,19 @@
+import { destroyCookie } from "nookies";
 import { useContext, useEffect } from "react"
 import { AuthContext } from "../context/AuthContext"
+import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
 import { api } from "../services/apiClient";
+import { AuthTokenError } from "../services/errors/AuthTokenError";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard(){
 
     const { user } = useContext(AuthContext);
+
+    const userCanSeeMetrics = useCan({
+        permissions: ['metrics.list']
+    });
 
     useEffect(() =>{
         api.get('/me')
@@ -14,15 +21,22 @@ export default function Dashboard(){
     },[])
 
     return(
-        <h1>Dashboard: {user?.email} </h1>
+        <>
+            <h1>Dashboard: {user?.email} </h1>
+
+            { userCanSeeMetrics && <div>Metricas</div>}
+        </>
     )
 }
 
 export const getServerSideProps = withSSRAuth( async (ctx) => {
     
     const apiClient = setupAPIClient(ctx);
-    const reponse = await apiClient.get('/me');
+
+    const response = await apiClient.get('/me');
     
+    console.log(response.data);
+
     return {
         props:{}
     }
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import axios, {AxiosError} from 'axios';
 import { parseCookies, setCookie } from 'nookies';
 import { signOut } from '../context/AuthContext';
+import { AuthTokenError } from './errors/AuthTokenError';
 
 let isRefreshing = false;
 let failedRequestsQueue: { onSuccess: (token: string) => void; onFailure: (err: AxiosError<any>) => void; }[] = [];
@@ -55,6 +56,8 @@ export function setupAPIClient(ctx = undefined){
     
                         if(process.browser) {
                             signOut();
+                        } else {
+                            return Promise.reject(new AuthTokenError());
                         }
                     }).finally(()=>{
                         isRefreshing = false;
